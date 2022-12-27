ac003586da94353ae0c9ada7f9cdfc441d9920cf
KID <50666207+KID421@users.noreply.github.com>
2022-12-27 21:19:46 +0800

    Update 認識腳本

---
+++ Assets/Scripts/FirstScript.cs
@@ -0,0 +1,13 @@
+﻿// 單行註解：說明、解釋、開發者等等的文字敘述
+// KID 2022.12.27 @3@
+// 程式不會執行註解
+// 程式上下、後面
+
+using UnityEngine;  // 引用命名空間(倉庫) Unity 遊戲引擎 (API)
+
+// class 類別 等同於腳本 Script
+// class 後面為腳本名稱，必須與檔案名稱相同
+public class FirstScript : MonoBehaviour
+{
+    // 腳本都會有一對大括號，放置腳本內容
+}
