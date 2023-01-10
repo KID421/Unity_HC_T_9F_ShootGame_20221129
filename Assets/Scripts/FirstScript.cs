36f9f6a12044547921b3e0e4e38fdf987cdb601e
KID <50666207+KID421@users.noreply.github.com>
2023-01-10 20:02:19 +0800

    Update 認識事件與欄位

--- Assets/Scripts/FirstScript.cs
+++ Assets/Scripts/FirstScript.cs
@@ -5,9 +5,48 @@
 
 using UnityEngine;  // 引用命名空間(倉庫) Unity 遊戲引擎 (API)
 
+// ※ 腳本為藍圖，不會執行
+// ※ 轉為物件才會執行，將腳本掛在場景物件上
+
 // class 類別 等同於腳本 Script
 // class 後面為腳本名稱，必須與檔案名稱相同
 public class FirstScript : MonoBehaviour
 {
+    // Tab 縮排：大括號下面一行
+    // 格式化：Ctrl + K D
+
     // 腳本都會有一對大括號，放置腳本內容
+
+    // 事件：窗口，讓程式開發者可以使用跟控制的程式
+    // 喚醒事件：播放遊戲時執行一次，大部分處理初始化
+    // 例如：LOL 發 500 給所有玩家
+    private void Awake()
+    {
+        // 此處要縮排兩次
+
+        // 輸出：測試，將訊息顯示在 Unity 的控制台 Console (Ctrl+Shift+C)
+        // 輸出("文字訊息")；
+        print("哈囉，沃德 :D");
+        print(1234567);
+        // Rich Text
+        print("<b>新年快樂~ 2023</b>");
+        print("<color=yellow>耶~</color>");
+        print("<color=#ff3355>粉紅色！</color>");
+    }
+
+    // 開始事件：在喚醒事件之後執行一次
+    private void Start()
+    {
+        print("<color=red>這是開始事件！</color>");
+
+        // 計算 1 + 2
+        print("輸出 1 + 2");
+        print($"輸出 {1 + 2}");
+    }
+
+    // 更新事件：一秒執行約 60 次，約 60 FPS
+    private void Update()
+    {
+        print("<color=#3333ff>這是更新事件！</color>");
+    }
 }
---
+++ Assets/Scripts/LearnField.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace KID
+{
+    /// <summary>
+    /// 摘要：不是必要的但很重要！
+    /// 學習欄位 Field
+    /// </summary>
+    public class LearnField : MonoBehaviour
+    {
+
+    }
+}
