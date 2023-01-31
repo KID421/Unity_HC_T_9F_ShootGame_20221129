e5f157eb571169ba92b74c933853aafd2d5129ff
KID <50666207+KID421@users.noreply.github.com>
2023-01-31 21:25:48 +0800

    Update 認識陣列與列舉

---
+++ Assets/Scripts/LearnArray.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace KID
+{
+    /// <summary>
+    /// 學習陣列
+    /// 儲存多個相同類型的資料
+    /// </summary>
+    public class LearnArray : MonoBehaviour
+    {
+        // 需求：經驗值
+        // Lv1 100
+        // Lv2 10000
+        // Lv3 1000000
+
+        public int lv1 = 100;
+        public int lv2 = 10000;
+        public int lv3 = 1000000;
+
+        // 陣列語法：
+        // 修飾詞 類型[] 名稱 指定 預設值；
+        public int[] lv;
+
+        // 道具陣列，五格
+        public GameObject[] props = new GameObject[5];
+
+        // 武器陣列，三格，刀子、石頭、樹枝
+        public string[] weapons = { "刀子", "石頭", "樹枝" };
+
+        private void Awake()
+        {
+            // 存取陣列資料
+            // 取得
+            // 陣列名稱[編號]
+            print("第二個武器：" + weapons[1]);
+            // print("第三個武器：" + weapons[3]);   // 錯誤，編號超出範圍
+
+            // 存放
+            // 陣列名稱[編號] 指定 值
+            weapons[2] = "寶特瓶";
+
+            // 陣列的數量
+            // 陣列名稱.長度
+            print("武器的數量：" + weapons.Length);
+            print("道具的數量：" + props.Length);
+        }
+    }
+}
---
+++ Assets/Scripts/LearnEnum.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace KID
+{
+    /// <summary>
+    /// 學習列舉 enumeration
+    /// </summary>
+    public class LearnEnum : MonoBehaviour
+    {
+        // 1. 定義列舉
+        // 2. 儲存在欄位
+
+        // 範例：季節 - 春夏秋冬
+        // 語法：修飾詞 列舉關鍵字 列舉名稱 { 選項 }
+        public enum Season
+        {
+            Spring, Summer, Fall, Winter
+        }
+
+        public Season gameSeason;
+
+        public Season season = Season.Summer;
+
+        private void Awake()
+        {
+            season = Season.Winter; // 將 season 改為 冬天
+
+            // 延遲呼叫 Invoke("方法名稱"，延遲秒數)；
+            Invoke("Spring", 5);                        // 五秒後呼叫春天方法
+        }
+
+        // 遊戲開始後五秒 season 變成春天
+        private void Spring()
+        {
+            season = Season.Spring;
+        }
+    }
+}
