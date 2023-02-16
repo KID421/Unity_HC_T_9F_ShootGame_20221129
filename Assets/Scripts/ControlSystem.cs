93cacd20fbd8ae9dcecf5c4bca0a19f6e7ba5057
KID <50666207+KID421@users.noreply.github.com>
2023-02-16 21:29:57 +0800

    Update 控制系統

--- Assets/Scripts/ControlSystem.cs
+++ Assets/Scripts/ControlSystem.cs
@@ -16,16 +16,43 @@ namespace KID
         private int speedMarble = 1500;
         [SerializeField, Header("發射間隔"), Range(0, 0.5f)]
         private float intervalShoot = 0.5f;
-        [SerializeField, Header("彈珠預置物")]
+        [SerializeField, Header("彈珠預製物")]
         private GameObject prefabMarble;
+        [SerializeField, Header("生成彈珠位置")]
+        private Transform pointSpawn;
+        [SerializeField, Header("箭頭")]
+        private GameObject objArrow;
 
         private string parAttack = "觸發攻擊";
         #endregion
 
         #region 事件
+        private void Awake()
+        {
+            SpawnMarble();
+        }
+
+        private void Update()
+        {
+            
+        }
         #endregion
 
         #region 方法
+        /// <summary>
+        /// 生成彈珠
+        /// </summary>
+        private void SpawnMarble()
+        {
+            for (int i = 0; i < countMarbleTotal; i++)
+            {
+                // Object.Instantiate(prefabMarble);       // 不簡寫
+
+                // 生成(物件，座標，角度)；
+                // 生成(彈珠預製物，生成位置的座標，生成位置的角度)；
+                Instantiate(prefabMarble, pointSpawn.position, pointSpawn.rotation);
+            }
+        }
         #endregion
     }
 }
