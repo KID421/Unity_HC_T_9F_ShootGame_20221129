ed94da81aa0e3794801e56740a33307cd3fddaa9
KID <50666207+KID421@users.noreply.github.com>
2023-03-02 20:06:13 +0800

    Update 添加發射彈珠

--- Assets/Scripts/ControlSystem.cs
+++ Assets/Scripts/ControlSystem.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;       // 條件 1
 
 namespace KID
 {
@@ -29,12 +30,12 @@ namespace KID
         #region 事件
         private void Awake()
         {
-            SpawnMarble();
+
         }
 
         private void Update()
         {
-            
+            InputManager();
         }
         #endregion
 
@@ -42,15 +43,49 @@ namespace KID
         /// <summary>
         /// 生成彈珠
         /// </summary>
-        private void SpawnMarble()
+        // 條件 2
+        private IEnumerator SpawnMarble()
         {
             for (int i = 0; i < countMarbleTotal; i++)
             {
                 // Object.Instantiate(prefabMarble);       // 不簡寫
 
                 // 生成(物件，座標，角度)；
-                // 生成(彈珠預製物，生成位置的座標，生成位置的角度)；
-                Instantiate(prefabMarble, pointSpawn.position, pointSpawn.rotation);
+                // 暫存彈珠 = 生成(彈珠預製物，生成位置的座標，生成位置的角度)；
+                GameObject tempMarble = Instantiate(prefabMarble, pointSpawn.position, pointSpawn.rotation);
+
+                // 取得元件<元件名稱>()
+                // GetComponent<元件名稱>()
+
+                // 暫存彈珠.取得元件<剛體>()
+                tempMarble.GetComponent<Rigidbody>().AddForce(0, 0, speedMarble);
+
+                // 條件 3
+                yield return new WaitForSeconds(intervalShoot);
+            }
+        }
+
+        /// <summary>
+        /// 輸入管理：偵測玩家的滑鼠 (觸控)
+        /// </summary>
+        private void InputManager()
+        {
+            // 如果 按下 左鍵 顯示箭頭
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                print("<color=#66ff99>玩家按下左鍵</color>");
+
+                objArrow.SetActive(true);                           // 顯示箭頭
+            }
+            // 如果 放開 左鍵 隱藏箭頭
+            else if (Input.GetKeyUp(KeyCode.Mouse0))
+            {
+                print("<color=#ff6699>玩家放開左鍵</color>");
+
+                objArrow.SetActive(false);                          // 隱藏箭頭
+
+                // 條件 4
+                StartCoroutine(SpawnMarble());                      // 生成彈珠
             }
         }
         #endregion
---
+++ Assets/Scripts/LearnCoroutine.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;       // 條件 1
+
+namespace KID
+{
+    /// <summary>
+    /// 學習協同程序 (協同或協程)
+    /// </summary>
+    public class LearnCoroutine : MonoBehaviour
+    {
+        // 作用：讓程式的時間停止
+        // 條件：
+        // 1. 引用系統集合命名空間
+        // 2. 定義一個傳回 IEnumerator 的方法
+        // 3. 方法內使用 yield return 時間
+        // 4. 使用 Start Coroutine 啟動
+
+        // 條件 2
+        private IEnumerator Test()
+        {
+            print("<color=#ff6699>第一行</color>");
+
+            // 條件 3
+            yield return new WaitForSeconds(2);
+
+            print("<color=#66ff99>第二行</color>");
+        }
+
+        private void Awake()
+        {
+            StartCoroutine(Test());     // 條件 4
+
+            StartCoroutine(Test2());
+        }
+
+        private IEnumerator Test2()
+        {
+            print("測試 1");
+
+            yield return new WaitForSeconds(3);
+
+            print("測試 2");
+
+            yield return new WaitForSeconds(2);
+
+            print("測試 3");
+        }
+    }
+}
