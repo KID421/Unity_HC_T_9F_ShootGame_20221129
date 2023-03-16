483b93811aad4a2f69004d00d400662e6f02842f
KID <50666207+KID421@users.noreply.github.com>
2023-03-16 21:21:59 +0800

    Update 添加受傷功能

--- Assets/Scripts/DamageSystemBasic.cs
+++ Assets/Scripts/DamageSystemBasic.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace KID
 {
@@ -7,8 +9,32 @@ namespace KID
     /// </summary>
     public class DamageSystemBasic : MonoBehaviour
     {
+        #region 資料
         [SerializeField, Header("基本資料")]
         private DataBasic data;
+        [SerializeField, Header("畫布傷害值")]
+        private GameObject prefabDamage;
+        [SerializeField, Header("圖片血條")]
+        private Image imgHp;
+        [SerializeField, Header("文字血量")]
+        private TextMeshProUGUI textHp;
+
+        private float hp, hpMax; 
+        #endregion
+
+        private void Awake()
+        {
+            // 解決腳本化物件資料不還原的方式
+            // ※ 將腳本化物件儲存於欄位內
+            hp = data.hp;
+
+            // 取得血量最大值
+            hpMax = data.hp;
+            // 文字血量 的 文字 = 資料 的 血量 轉為字串
+            textHp.text = data.hp.ToString();
+
+            GetDamage(50);
+        }
 
         /// <summary>
         /// 受傷
@@ -16,7 +42,12 @@ namespace KID
         /// <param name="damage">受到的傷害值</param>
         private void GetDamage(float damage)
         {
+            // 扣血並且更新介面文字與圖片
+            hp -= damage;
 
+            textHp.text = hp.ToString();
+            // 公式：當前血量 / 血量最大值 (0 ~ 1 百分比)
+            imgHp.fillAmount = hp / hpMax;
         }
 
         /// <summary>
