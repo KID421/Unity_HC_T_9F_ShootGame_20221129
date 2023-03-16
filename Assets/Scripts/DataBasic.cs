5d34284028a1f6fff4c91cfed5d7f18767597b7a
KID <50666207+KID421@users.noreply.github.com>
2023-03-16 18:50:20 +0800

    Update 建立基本資料

---
+++ Assets/Scripts/DataBasic.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace KID
+{
+    /// <summary>
+    /// 基本資料：血量與攻擊力
+    /// </summary>
+    public class DataBasic : MonoBehaviour
+    {
+        [Header("血量"), Range(0, 10000)]
+        public float hp = 200;
+        [Header("攻擊力"), Range(10, 5000)]
+        public float attack = 50;
+    }
+}
