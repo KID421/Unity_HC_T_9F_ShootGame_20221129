ff1445dc94f466e3d71508b7dd28a07085dfc5dd
KID <50666207+KID421@users.noreply.github.com>
2023-03-16 20:02:26 +0800

    Update 添加血條畫布

--- Assets/Scripts/DamageSystemBasic.cs
+++ Assets/Scripts/DamageSystemBasic.cs
@@ -7,6 +7,32 @@ namespace KID
     /// </summary>
     public class DamageSystemBasic : MonoBehaviour
     {
+        [SerializeField, Header("基本資料")]
+        private DataBasic data;
 
+        /// <summary>
+        /// 受傷
+        /// </summary>
+        /// <param name="damage">受到的傷害值</param>
+        private void GetDamage(float damage)
+        {
+
+        }
+
+        /// <summary>
+        /// 顯示傷害值物件
+        /// </summary>
+        private void ShowDamage()
+        {
+
+        }
+
+        /// <summary>
+        /// 死亡
+        /// </summary>
+        private void Dead()
+        {
+
+        }
     }
 }
