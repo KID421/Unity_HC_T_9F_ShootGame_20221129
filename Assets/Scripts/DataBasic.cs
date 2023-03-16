69662190eef5458e2160f7f16d9a44655986d520
KID <50666207+KID421@users.noreply.github.com>
2023-03-16 19:03:24 +0800

    Update 改為腳本化物件

--- Assets/Scripts/DataBasic.cs
+++ Assets/Scripts/DataBasic.cs
@@ -5,7 +5,14 @@ namespace KID
     /// <summary>
     /// 基本資料：血量與攻擊力
     /// </summary>
-    public class DataBasic : MonoBehaviour
+    
+    // 腳本化物件 ScriptabaleObject
+    // 1. 將繼承類別改為 ScriptabaleObject
+    // 2. 添加類別屬性 CreateAssetMenu
+    // 3. 屬性內使用 menuName 自訂菜單名稱
+
+    [CreateAssetMenu(menuName = "KID/Data Basic")]
+    public class DataBasic : ScriptableObject
     {
         [Header("血量"), Range(0, 10000)]
         public float hp = 200;
