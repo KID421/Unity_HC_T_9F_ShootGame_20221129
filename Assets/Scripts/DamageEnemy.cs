41fe2626920247958bc36df907ffbd61876fadca
KID <50666207+KID421@users.noreply.github.com>
2023-03-28 20:07:30 +0800

    Update 添加敵人傷害系統

---
+++ Assets/Scripts/DamageEnemy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace KID
+{
+    /// <summary>
+    /// 敵人受傷系統
+    /// 1. 碰到彈珠會受傷
+    /// 2. 死亡會機率性掉落金幣
+    /// 3. 死亡會刪除物件
+    /// </summary>
+    public class DamageEnemy : DamageSystemBasic
+    {
+
+    }
+}
--- Assets/Scripts/DamageSystemBasic.cs
+++ Assets/Scripts/DamageSystemBasic.cs
@@ -19,7 +19,11 @@ namespace KID
         [SerializeField, Header("文字血量")]
         private TextMeshProUGUI textHp;
 
-        private float hp, hpMax; 
+        private float hp, hpMax;
+
+        [SerializeField, Header("動畫控制器")]
+        private Animator ani;
+        private string parDamage = "觸發受傷";
         #endregion
 
         private void Awake()
@@ -33,7 +37,7 @@ namespace KID
             // 文字血量 的 文字 = 資料 的 血量 轉為字串
             textHp.text = data.hp.ToString();
 
-            GetDamage(50);
+            GetDamage(99999);
         }
 
         /// <summary>
@@ -42,20 +46,41 @@ namespace KID
         /// <param name="damage">受到的傷害值</param>
         private void GetDamage(float damage)
         {
+            // 設定觸發器(觸發器名稱)
+            ani.SetTrigger(parDamage);
+
             // 扣血並且更新介面文字與圖片
             hp -= damage;
+
+            // 如果 血量 <= 0 就呼叫死亡方法
+            // 判斷式 如果只有一行程式可以省略大括號
+            if (hp <= 0) Dead();
 
             textHp.text = hp.ToString();
             // 公式：當前血量 / 血量最大值 (0 ~ 1 百分比)
             imgHp.fillAmount = hp / hpMax;
+
+            ShowDamage(damage);
         }
 
         /// <summary>
         /// 顯示傷害值物件
         /// </summary>
-        private void ShowDamage()
+        private void ShowDamage(float damage)
         {
+            // 生成的物件 = 生成傷害值物件
+            GameObject temp = Instantiate(
+                prefabDamage,
+                transform.position + new Vector3(0, 1, 0),
+                Quaternion.Euler(60, 0, 0));
 
+            // 取得生成的物件 子物件 並更新 文字
+            // transform.GetChild(0) 取得第一個子物件
+            temp.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "-" + damage;
+
+            // 一秒後 刪除 生成的物件
+            // 刪除(物件，延遲時間)
+            Destroy(temp, 1);
         }
 
         /// <summary>
@@ -63,7 +88,7 @@ namespace KID
         /// </summary>
         private void Dead()
         {
-
+            hp = 0;         // 血量歸零
         }
     }
 }
